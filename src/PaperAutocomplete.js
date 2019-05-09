3c0a1d188a86f81e90c233636963888cb714938e
tpluscode <tpluscode@users.noreply.github.com>
2019-05-09 08:04:50 +0200

    chore: lint setup

--- src/PaperAutocomplete.js
+++ src/PaperAutocomplete.js
@@ -3,47 +3,47 @@ import { html, css, LitElement } from 'lit-element';
 export default class PaperAutocomplete extends LitElement {
   static get styles() {
     return css`
-    :host {
-      display: flex;
-      flex-flow: wrap;
-      position: relative;
-    }
+      :host {
+        display: flex;
+        flex-flow: wrap;
+        position: relative;
+      }
 
-    [hidden] {
-      display: none;
-    }
+      [hidden] {
+        display: none;
+      }
 
-    paper-listbox {
-      max-height: 250px;
-      overflow: scroll;
-      width: 90%;
-      position: absolute;
-      background-color: white;
-      opacity: 1;
-      z-index: 1;
-      top: 55px;
-      left: 0;
-      box-shadow: 3px 3px 3px grey;
-    }
-    
-    paper-listbox paper-item {
-      cursor: pointer
-    }
+      paper-listbox {
+        max-height: 250px;
+        overflow: scroll;
+        width: 90%;
+        position: absolute;
+        background-color: white;
+        opacity: 1;
+        z-index: 1;
+        top: 55px;
+        left: 0;
+        box-shadow: 3px 3px 3px grey;
+      }
 
-    #search {
-      flex: 1 65%
-    }
+      paper-listbox paper-item {
+        cursor: pointer;
+      }
+
+      #search {
+        flex: 1 65%;
+      }
     `;
   }
 
   static get properties() {
     return {
-      showResults: {type: Boolean},
+      showResults: { type: Boolean },
       value: { type: String, notify: true },
-      label: {type:String},
-      description: {type:String},
-      searchTemplate: {type:String},
-    }
+      label: { type: String },
+      description: { type: String },
+      searchTemplate: { type: String },
+    };
   }
 
   constructor() {
